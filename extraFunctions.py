950d069fffc9b7a47ae696ee4abd588bb6e71940
racarlos <racarlos1@up.edu.ph>
2022-05-05 10:34:22 -0400

    merge helperfunctions.py with subset number calculator function

--- extraFunctions.py
+++ extraFunctions.py
@@ -51,3 +51,46 @@ def getSolutionIcon(solutionType):
         solutionIcon = 'close-circle'
     return solutionIcon
    
+# Get the indexes to be computed per thread calculation
+def getSubsetComputationAllocation(vulnCount, coreCount):
+
+    # Add Responsibilities based on number of Cores
+    responsibilities = []
+
+    for i in range(coreCount): 
+        responsibilities.append([])
+
+    index = 0
+    flip = False
+    for i in range(vulnCount):
+
+        if flip is False:
+            responsibilities[index].append(i)
+            index += 1 
+        elif flip is True:
+            responsibilities[index].append(i)
+            index -= 1
+
+
+        if index == coreCount and flip is False:
+            index = 3
+            flip = True
+            continue 
+        elif index == -1 and flip is True:
+            index = 0
+            flip = False
+        continue
+
+    return responsibilities
+
+# vulnCount = 60
+# coreCount = 4
+
+# responsibilities = getSubsetComputationAllocation(vulnCount,coreCount)
+
+# for arr in responsibilities:
+#     sum = 0
+#     for i in range(len(arr)):
+#         sum += arr[i]
+
+#     print(f'{responsibilities.index(arr)}: {sum}')
